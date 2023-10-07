11c3014ea0f617843a0f0f45fab1175f84f3e439
hazemanwer2000 <hazemanwer2000@proton.me>
2023-10-08 00:26:22 +0300

    Image Path Script!

--- .scripts/image_path.py
+++ .scripts/image_path.py
@@ -2,9 +2,17 @@
 import os
 import json
 import re
+from pprint import pprint
 
 # Note: Depends on the script's path in the repo.
 repo_dir = os.path.dirname(os.path.dirname(__file__))
+img_dir_name = '.img'
+
+# GLOBAL: Used to store all referenced image names
+ref_imgs = []
+
+# GLOBAL: Used to store all actually existing image names
+exist_imgs = os.listdir(os.path.join(repo_dir, img_dir_name))
 
 # Capture image name
 def capture_img_name(html_tag):
@@ -40,8 +48,9 @@ def process_file(file_path):
     # Loop on each relevant title cell
     for cell in img_cells:
         img_name = capture_img_name(cell['source'][0])
+        ref_imgs.append(img_name)                           # Referenced (i.e, used) elsewhere
         depth = get_depth_of_file(file_path)
-        proper_path = ('../' * depth) + '.img/' + img_name
+        proper_path = ('../' * depth) + img_dir_name + '/' + img_name
         cell['source'][0] = replace_img_path(cell['source'][0], proper_path)
 
     # Write 'JSON' back to file
@@ -56,4 +65,26 @@ def process_directory(directory):
             if os.path.splitext(file)[1] == '.ipynb':
                 process_file(file_path)
 
+# Update '.ipynb' files
 process_directory(repo_dir)
+
+ref_imgs = set(ref_imgs)
+exist_imgs = set(exist_imgs)
+
+ref_minus_exist = ref_imgs - exist_imgs
+exist_minus_ref = exist_imgs - ref_imgs
+
+nothingToReport = True
+
+if (len(ref_minus_exist) > 0):
+    print("\n[INFO] These images are referenced, yet seem to be missing.")
+    print("\n".join(ref_minus_exist))
+    nothingToReport = False
+
+if (len(exist_minus_ref) > 0):
+    print("\n[INFO] These images exist, but aren't referenced anywhere.")
+    print("\n".join(exist_minus_ref))
+    nothingToReport = False
+
+if (nothingToReport):
+    print("\n[INFO] Updated image path(s) successfully.")
